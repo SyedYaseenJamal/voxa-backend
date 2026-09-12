5b6db47966b04bc810051adfdc353dac2172d2c9
jawwadkareem <karimjawwad09@gmail.com>
2026-09-12 18:38:03 +0500

    Pipeine and recordig and recording anaylsis api modified

--- src/modules/telephony/telephony.service.js
+++ src/modules/telephony/telephony.service.js
@@ -8,7 +8,7 @@ function pipelineFetch(url, options = {}) {
   return fetch(url, { ...options, dispatcher: pipelineAgent });
 }
 
-const PIPELINE_URL = (process.env.VOXA_AI_PIPELINE_URL || 'https://crm-intelligence-voxa.vercel.app').replace(/\/$/, '');
+const PIPELINE_URL = 'https://crm-intelligence-voxa.vercel.app'.replace(/\/$/, '');
 const RECORDINGS_DIRS = Array.from(new Set([
   process.env.RECORDINGS_PATH,
   path.resolve(process.cwd(), 'recordings'),
